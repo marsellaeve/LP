47debefbf950f68e4c6a7442d03880faf6a80bfc
qqdnada <nada.qori@gmail.com>
2019-11-12 12:21:09 +0800

    update (2)

--- app/src/main/java/id/ac/its/evelyn/applp/DeskripsiActivity.java
+++ app/src/main/java/id/ac/its/evelyn/applp/DeskripsiActivity.java
@@ -1,21 +1,15 @@
 package id.ac.its.evelyn.applp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class DeskripsiActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_login);
-    }
-
-    public void login(){
-        Intent intent= new Intent(LoginActivity.this, DeskripsiActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_deskripsi);
     }
 }
